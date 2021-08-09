349a0e3550c33c87849db7fb79bbc9d2254c7b46
iacopo <iacopo.iannace@icloud.com>
2021-08-09 22:27:16 +0200

    Add mapScreen & Navigation bar

--- App.js
+++ App.js
@@ -3,13 +3,37 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { Provider } from 'react-redux';
 import { store } from "./store.js";
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import 'react-native-gesture-handler';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 import HomeScreen from './screens/HomeScreen.js';
+import MapScreen from './screens/MapScreen.js'
 
 export default function App() {
+
+  const Stack = createNativeStackNavigator();
+
   return (
     <Provider store={store}>
-      <HomeScreen />
+      <NavigationContainer>
+        <SafeAreaProvider>
+          <Stack.Navigator>
+            <Stack.Screen 
+              name="HomeScreen"
+              component={HomeScreen}
+              options={{headerShown: false}}
+            />
+            <Stack.Screen 
+              name="MapScreen"
+              component={MapScreen}
+              options={{headerShown: false}}
+            />
+          </Stack.Navigator>
+        </SafeAreaProvider>
+      </NavigationContainer>
+      
     </Provider>
   );
 }
---
+++ screens/MapScreen.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { StyleSheet, Text, View, SafeAreaView } from 'react-native'
+
+const MapScreen = () => {
+    return (
+        <SafeAreaView>
+            <View>
+                <Text>Mapstuff</Text>
+            </View>
+        </SafeAreaView>
+        
+    )
+}
+
+export default MapScreen
+
+const styles = StyleSheet.create({})
